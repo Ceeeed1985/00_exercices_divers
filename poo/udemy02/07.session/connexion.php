651a3bb3514f990c455459ff38ca0f6f5b468349
Cedric Truyen <cedrictruyen@icloud.com>
2024-09-12 16:58:26 +0200

    avancement dans les sessions

--- poo/udemy02/07.session/connexion.php
+++ poo/udemy02/07.session/connexion.php
@@ -10,7 +10,34 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 </head>
 <body>
-    <form action="connexion.php" method="POST">
+
+<?php
+    require_once('SessionManager.php');
+    require_once('User.php');
+
+    SessionManager::startSession();
+    if(isset($_POST['connexion'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if($email === 'john@gmail.com' && $password === 'password1234'){
+            $user = new User($email);
+            SessionManager::setUser($user);
+
+            header('location: dashboard.php');
+            exit();
+
+        } else {
+            $error = "Identifiant invalide";
+        }
+    }
+    if (isset($error)){
+        echo $error;
+    }
+
+?>
+
+<form action="connexion.php" method="POST">
         <h2>Formulaire de connexion</h2>
         <label for="email">Email : </label>
         <input class="inputEmail" type="email" id="email" name="email" required>
