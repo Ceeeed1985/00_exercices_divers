1bf781301d333562e531d133e3db8554506f6985
Cedric Truyen <cedrictruyen@icloud.com>
2024-09-13 14:28:47 +0200

    suite des exercices sur les magic methods

--- poo/udemy02/08.magicMethods/Users.php
+++ poo/udemy02/08.magicMethods/Users.php
@@ -2,7 +2,7 @@
 
 class Users
 {
-    private $name;
+    public $name;
     private $email;
     private $age;
     private $favoriteAnimal;
@@ -16,15 +16,43 @@ class Users
         $this->setFavoriteAnimal($favoriteAnimal);
     }
 
-    //Méthode magiques
+    //Méthode magique __cal()
 
     public function __call($method, $param)
     {
-        echo 'La méthode '. $method . ' n\'est pas accessible ou n\'existe pas';
+        echo 'La méthode <strong>< '. $method . ' ></strong> n\'est pas accessible ou n\'existe pas';
+        echo '<br>';
         echo 'Arguments : ' . implode(', ', $param);
     }
 
+    //Méthode magique __get()
+    //Elle va s'executer si nous essayons d'accéder à un attribut qui n'existe pas, ou non accessible dans une classe
+    public function __get($attribut)
+    {
+        echo "L'attribut <strong>< ". $attribut . " ></strong> auquel vous essayer d'accéder n'existe pas ou est inaccessible";
+    }
+ 
+    //Méthode magique __set()
+    //Elle va s'executer si nous essayons de créer ou de mettre à jour un attribut qui n 'existe pas ou non accessible dans une classe;
+    public function __set($attribut, $value)
+    {
+        echo "Impossible de mettre à jour l'attribut <strong>< " . $attribut . " ></strong> avec la valeur <strong>< " . $value . " ></strong>. <br>Attribut non accessible";
+    }
 
+
+    //Méthode magique __isset()
+    //Elle va s'executer de manière automatique lorsque la fonction isset() est appelée sur des attributs qui sont inaccessibles ou inexistants
+    public function __isset($attribut)
+    {
+        echo "L'attribut <strong>< " . $attribut . " ></strong> n'existe pas ou est inaccessible";
+    }
+
+
+   //Méthode magique __unset()
+    public function __unset($attribut)
+    {
+        echo "L'attribut <strong>< " . $attribut . " ></strong> que vous voulez détruire n'existe pas ou est inaccessible";
+    }
 
 
 
--- poo/udemy02/08.magicMethods/index.php
+++ poo/udemy02/08.magicMethods/index.php
@@ -3,13 +3,39 @@
 
 require_once('Users.php');
 
-$Magali = new Users ("Magali", "mdv@gmail.com", 37, "Dauphin");
-// $Magali->showUser();
-
-// $Magali->setNationality("Belgian");
-// var_dump($Magali);
-// $Magali->showUser();
+$user1 = new Users ("Magali", "mdv@gmail.com", 37, "Dauphin");
 
 echo '<br><br>';
 
-$Magali->listerMoi('PHP', 'Programmation');
+
+echo "<b>Exemple sur la magic method __call() </b><br>";
+$user1->listerMoi('PHP', 'Programmation');
+
+echo '<br><br>';
+
+echo "<b>Exemple sur la magic method __get() </b><br>";
+echo $user1->name;
+echo '<br>';
+echo $user1->adresse;
+
+echo '<br><br>';
+
+echo "<b>Exemple sur la magic method __set() </b><br>";
+$user1->age = 38;
+echo '<br>';
+$user1->coucou = 'hello';
+
+echo '<br><br>';
+
+echo "<b>Exemple sur la magic method __isset() </b><br>";
+if (isset($user1->age))
+{
+    echo "Existe";
+} else {
+    echo "<br>N'existe pas";
+}
+
+echo '<br><br>';
+
+echo "<b>Exemple sur la magic method __unset() </b><br>";
+unset($user1->age);
